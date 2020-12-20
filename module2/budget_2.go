71a6e60d2fc154472d6a6f5b0932a03ca97b7691
Stephen Harris <contact@stephenharris.info>
2020-12-20 12:43:25 +0000

    module 2

--- module2/budget_2.go
+++ module2/budget_2.go
@@ -54,6 +54,13 @@ var errDuplicateEntry = errors.New("Cannot add duplicate entry")
 // AddItem adds an item to the current budget
 func (b *Budget) AddItem(description string, price float32) error {
 
+	if b.CurrentCost()+price > b.Max {
+		return errDoesNotFitBudget
+	}
+
+	newItem := Item{Description: description, Price: price}
+	b.Items = append(b.Items, newItem)
+
 	return nil
 }
 
@@ -61,7 +68,8 @@ func (b *Budget) AddItem(description string, price float32) error {
 func (b *Budget) RemoveItem(description string) {
 	for i := range b.Items {
 		if b.Items[i].Description == description {
-
+			b.Items = append(b.Items[:i], b.Items[i+1:]...)
+			break
 		}
 	}
 }
@@ -70,12 +78,27 @@ func (b *Budget) RemoveItem(description string) {
 func CreateBudget(month time.Month, max float32) (*Budget, error) {
 	var newBudget *Budget
 
+	_, hasEntry := report[month]
+
+	if len(report) >= 12 {
+		return newBudget, errReportIsFull
+	}
+	if hasEntry {
+		return newBudget, errDuplicateEntry
+	}
+
+	newBudget = &Budget{Max: max}
+	report[month] = newBudget
+
 	return newBudget, nil
 }
 
 // GetBudget returns budget for given month
 func GetBudget(month time.Month) *Budget {
-
+	budget, ok := report[month]
+	if ok {
+		return budget
+	}
 	return nil
 }
 
